d2b6d4ec6131dbe0c77d3b3f6f5e6e674e34b6ba
Alex Schnabl <alex.schnabl02@gmail.com>
2023-08-10 22:37:32 +0200

    test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,17 @@
-<script>
-	export let data;
-</script>
+<script lang="ts">
+    export let data;
 
-<ul class="text-5xl">
-	{#each data.countries as country}
-		<li>{country.name}</li>
-	{/each}
-</ul>
+    let clearText: string;
+    let encrypted: string = "";
+
+    function encrypt() {
+        encrypted = clearText;
+    }
+</script>
+<div class="p-8">
+    <div class="flex gap-2">
+        <input bind:value={clearText} class="px-4 py-2 border">
+        <button on:click={encrypt} class="bg-neutral-900 p-4 text-white font-medium">Encrypt</button>
+    </div>
+    <span>{encrypted}</span>
+</div>
