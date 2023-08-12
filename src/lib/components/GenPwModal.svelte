dc73b8f7087da3d036033bd74844b632b049a870
BlackPhoenix134 <46650884+BlackPhoenix134@users.noreply.github.com>
2023-08-12 12:24:45 +0200

    cleanup

--- src/lib/components/GenPwModal.svelte
+++ src/lib/components/GenPwModal.svelte
@@ -1,18 +1,15 @@
 <script lang="ts">
     import {text} from "@sveltejs/kit";
+    import GenPw from "$lib/components/GenPw.svelte";
 
     let isModalOpen = false
     let thisOnConfirm: Callback;
 
     type Callback = (password: string) => void;
     let generatedPassword: string;
-    let length: number
-    let useSpecialCharacters: boolean
 
     export function show(onConfirm: Callback) {
         generatedPassword = "";
-        length = 14
-        useSpecialCharacters = true
         isModalOpen = true;
         thisOnConfirm = onConfirm;
     }
@@ -27,32 +24,16 @@
     }
 
 
-    async function generatePassword() {
-        const buffer = new Uint8Array(length);
-        const crypto = window.crypto || (window as any).msCrypto; // For compatibility with IE11.
-        const array = await crypto.getRandomValues(buffer);
-        let password = '';
-        let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-        if (useSpecialCharacters)
-            characters += "!§$%&/()=?"
-        for (let i = 0; i < length; i++) {
-            password += characters.charAt(array[i] % characters.length);
-        }
-        generatedPassword = password;
+    function onPwChanged(event: any) {
+        const pw = event.detail.password
+        generatedPassword = pw;
     }
 </script>
 
 <div class="modal" class:modal-open={isModalOpen}>
-    <div class="modal-box">
+    <div class="modal-box h-[60vh] w-[95vw] md:h-[40vh] md:w-[50vw]">
         <h3 class="font-bold text-lg">Generate Password</h3>
-        <div class="flex gap-4">
-            <label>Length</label>
-            <input type="number" bind:value={length}>
-            <label>Special Characters</label>
-            <input type="checkbox" bind:value={useSpecialCharacters}>
-            <button on:click={generatePassword}>Generate</button>
-        </div>
-        <div>{generatedPassword}</div>
+        <GenPw on:changed={onPwChanged}/>
         <div class="modal-action">
             <button class="btn" on:click={closeCancel}>Cancel</button>
             <button class="btn" on:click={closeConfirm}>Pazzwordz</button>
