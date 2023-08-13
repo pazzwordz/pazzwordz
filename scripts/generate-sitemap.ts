d02e606ab405d11cfb02ff08eddc8c89f7a28a84
Alex Schnabl <alex.schnabl02@gmail.com>
2023-08-14 00:20:49 +0200

    generate sitemap

--- scripts/generate-sitemap.ts
+++ scripts/generate-sitemap.ts
@@ -43,4 +43,4 @@ getEndpoints(tree, baseRoute);
 
 const sitemap = getSitemapXML("https://www.pazzwordz.io", routes)
 
-fs.writeFileSync('./vercel/output/sitemap.xml', sitemap);
+fs.writeFileSync('/vercel/output/sitemap.xml', sitemap);
