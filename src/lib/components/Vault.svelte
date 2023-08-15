427e56cdd624d0b9307ff8fcd5d143eaaef9de3c
Alex Schnabl <alex.schnabl02@gmail.com>
2023-08-15 11:35:59 +0200

    cleaned up css

--- src/lib/components/Vault.svelte
+++ src/lib/components/Vault.svelte
@@ -211,7 +211,7 @@
 <ImportReportModal bind:this={importReportModal}/>
 <SharePwModal bind:this={sharePwModal} supabase={supabase} userId={userId+""}/>
 
-<div class="w-full flex flex-col lg:flex-row gap-4 lg:p-8 h-[80vh]">
+<div class="w-full flex flex-col lg:flex-row gap-4 lg:p-8 min-h-[80vh]">
     <div class="lg:w-1/5 flex flex-col items-center gap-4">
         <a class="btn w-64 btn-outline flex gap-2 items-center" href={routes.cloud.cloud}>
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="stroke-current"
